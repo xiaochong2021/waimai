cd8bea98b005502f73be544cdcb27799c45fd66e
xiaochong2021 <a811159720@qq.com>
2020-05-21 00:33:04 +0800

    init waimai

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
+/*
+* 入口js
+* */
+
 import Vue from 'vue'
 import App from './App.vue'
-
-Vue.config.productionTip = false
+import router from './router'
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el:'#app',
+  render:h => h(App),
+  router
+})
